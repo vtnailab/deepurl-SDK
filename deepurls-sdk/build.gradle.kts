db0d5f40823dab321e55ff6918f5dd414116aca2
alex <kushagra@voicetonotes.ai>
2026-01-26 03:25:16 +0530

    fixed

--- deepurls-sdk/build.gradle.kts
+++ deepurls-sdk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -9,7 +10,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -37,11 +37,17 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+}
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("com.android.installreferrer:installreferrer:2.2")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.vtnailab"
+                artifactId = "deepurls-sdk"
+                version = "1.0.0"
+            }
+        }
+    }
 }
