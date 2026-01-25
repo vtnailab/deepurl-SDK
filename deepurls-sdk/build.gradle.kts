c5871209424c99804fba14c98ce94f27db922dd2
alex <kushagra@voicetonotes.ai>
2026-01-26 03:50:32 +0530

    fixed

--- deepurls-sdk/build.gradle.kts
+++ deepurls-sdk/build.gradle.kts
@@ -37,6 +37,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.android.installreferrer:installreferrer:2.2")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
 
 afterEvaluate {
